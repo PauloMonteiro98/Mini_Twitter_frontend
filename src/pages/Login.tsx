fb90b6bcab9e4a0522a140e7bf5589433b7ac9a8
joão paulo <jpcabal2@hotmail.com>
2026-03-16 12:01:07 -0300

    feature: create login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-[#0B1120] px-4 font-sans text-white">
+      <h2>Login</h2>
+    </div>
+  );
+}
