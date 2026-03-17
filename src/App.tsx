e51a021e63d666e5a7295ca2619380cac76e07c4
joão paulo <jpcabal2@hotmail.com>
2026-03-17 09:27:16 -0300

    feature: add react-query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="*"
-          element={
-            <div className="flex min-h-screen items-center justify-center bg-[#0B1120] text-white">
-              <h1 className="text-2xl">404 - Página não encontrada</h1>
-            </div>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="*"
+            element={
+              <div className="flex min-h-screen items-center justify-center bg-[#0B1120] text-white">
+                <h1 className="text-2xl">404 - Página não encontrada</h1>
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
